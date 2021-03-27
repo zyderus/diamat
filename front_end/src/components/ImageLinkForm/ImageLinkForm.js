a24adffdc19786ad7f0b603a1620e2ce13e349a5
zyderus <zyderus@gmail.com>
2021-03-27 22:13:46 +0500

    add backend

--- front_end/src/components/ImageLinkForm/ImageLinkForm.js
+++ front_end/src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,8 +10,11 @@ export class ImageLinkForm extends Component {
         </p>
         <div className='w-80 center'>
           <div className='form pa4 br3 shadow-5'>
-            <input className="f4 pa2 w-70 center" type="text"/>
-            <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+            <input onChange={this.props.onInputChange} className="f4 pa2 w-70 center" type="text"/>
+            <button 
+              onClick={this.props.onSubmit} 
+              className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            >Detect</button>
           </div>
         </div>
       </div>
